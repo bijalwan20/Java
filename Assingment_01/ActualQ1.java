67edfcdc0742f57523ff500cc675ef0211689b5e
bijalwan20 <102857723+bijalwan20@users.noreply.github.com>
2022-05-10 09:46:08 +0530

    Update ActualQ1.java

--- Assingment_01/ActualQ1.java
+++ Assingment_01/ActualQ1.java
@@ -11,16 +11,15 @@ public class ActualQ1 {
 
 
 	public static void main(String[] args) {
-		Student obj = new Student("Ramesh", 14, 'H', 88);
-		Student obj1 = new Student("Dinesh", 14, 'H', 78);
-		Student obj2 = new Student("Dhanesh", 12, 'H', 98);
-		Student obj3 = new Student("Chandresh", 15, 'H', 38);
-		Student obj4 = new Student("Beach", 69, 'H', 58);
+		Student obj = new Student("suman", 14, 'H', 88);
+		Student obj1 = new Student("ram", 14, 'H', 78);
+		Student obj2 = new Student("shiv", 12, 'H', 98);
+		Student obj3 = new Student("Chand", 15, 'H', 38);
+		Student obj4 = new Student("naruto", 69, 'H', 58);
 		Student obj5 = new Student("Laila", 18, 'H', 98);
 		//Getting the result 
 		Student result = new Student();
 		result.get_avg();
-		
 
 	}
 
